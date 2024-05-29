f6911297ce4c798346ac19cbd8d8dd0d5530628e
Brumassio <dibrunspeed@gmail.com>
2024-05-29 09:43:10 -0300

    Começando a implementar o tabuleiro

---
+++ chess-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        private Peca[,] Pecas { get; set; }
+
+        public Tabuleiro(int lines, int columns)
+        {
+            Lines=lines;
+            Columns=columns;
+            Pecas = new Peca[Lines, Columns];
+        }
+    }
+}
