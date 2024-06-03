2332efcd66fed32cf6eca06c65c718bed8567a87
Brumassio <dibrunspeed@gmail.com>
2024-06-03 16:18:35 -0300

    Criando a classe PosicaoXadrez

---
+++ chess-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+
+namespace Xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public PosicaoXadrez(char column, int line)
+        {
+            Column=column;
+            Line=line;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(Line - 8, Column - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
